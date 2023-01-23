b86329ffa336b350597c64a7c844b36813dd958d
Mark Gowarty-Weinstock <dev.markgw@gmail.com>
2023-01-23 13:26:25 -0700

    add class assingments

---
+++ Assignments_2420/tests/a01/A01Part1Test.java
@@ -0,0 +1,14 @@
+package a01;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class A01Part1Test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
