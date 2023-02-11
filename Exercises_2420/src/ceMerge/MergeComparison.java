f7489ab85e35cbcaae733209639c4ca9715a8ce4
Mark Gowarty-Weinstock <dev.markgw@gmail.com>
2023-02-10 17:15:14 -0700

    feat: add merge sort slow class and comparison class

---
+++ Exercises_2420/src/ceMerge/MergeComparison.java
@@ -0,0 +1,43 @@
+package ceMerge;
+
+import edu.princeton.cs.algs4.Merge;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class MergeComparison {
+	public static void main(String[] args) {
+		int BILLION = 1_000_000_000;
+		
+		int n = 10_000;
+		
+		for(int i = 0; i < 10; i++) {
+			Integer[] a = getIntegerArray(n);
+			
+			long start = System.nanoTime();
+			Merge.sort(a);
+			double elapsedTime = System.nanoTime() - start;
+			
+			StdOut.printf("Time to sort " + n + " elements = %.4f s %n", elapsedTime / BILLION);
+			
+			n*=2;
+		}
+		
+	}
+
+	/**
+	 * Creates an Integer array of the specified size filled with random seven digit number
+	 * @param size
+	 * @return
+	 */
+	private static Integer[] getIntegerArray(int size) {
+		Integer[] integers = new Integer[size];
+		
+		
+		
+		for(int i = 0; i < integers.length; i++) {
+			integers[i] = StdRandom.uniformInt(1000000, 9000000);
+		}
+		
+		return integers;
+	}
+}
